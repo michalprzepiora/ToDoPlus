e3b8392d4e16a3684abd67a33f78e326e6ef800a
Michal Przepiora <mprzepiora@ptc.com>
2022-10-10 15:04:56 +0200

    Initial commit

---
+++ src/main/java/pl/com/przepiora/ToDoPlus/ToDoPlusApplication.java
@@ -0,0 +1,13 @@
+package pl.com.przepiora.ToDoPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/przepiora/ToDoPlus/ToDoPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.przepiora.ToDoPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
