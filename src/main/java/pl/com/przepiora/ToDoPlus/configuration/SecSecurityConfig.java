a003bca48febe23d140a192b0e8e519c903f6280
przepioram <michal.przepiora@gmail.com>
2022-10-24 21:35:52 +0200

    add simple In Memory Authentication

---
+++ src/main/java/pl/com/przepiora/ToDoPlus/configuration/SecSecurityConfig.java
@@ -0,0 +1,29 @@
+package pl.com.przepiora.ToDoPlus.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        // http builder configurations for authorize requests and form login (see below)
+//    }
+}
